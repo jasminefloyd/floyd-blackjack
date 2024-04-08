108b5c7b754b545e331ed0c419c5274a5350de7b
jasminefloyd <42697356+jasminefloyd@users.noreply.github.com>
2024-04-08 16:32:10 -0400

    new BJ updates

--- assets/index.js
+++ assets/index.js
@@ -6,32 +6,48 @@ var mainAppContainerEl = document.getElementById('app-container-main')
 var startAppContainerEl = document.getElementById('app-container-start')
 var cardsEl = document.getElementById('cards-main')
 var drawBtnEl = document.getElementById('draw-btn')
-var firstCard
-var secondCard
-var cards = []
-//mainAppContainerEl.style.display = `none`
+
+mainAppContainerEl.style.display = `none`
 
 
-function checkBlackjack(){
-    if (sum === 21) {
-        hasBlackjack = true
-        startGame()
-    } 
-}
 
 
-function dealCards(){
-    firstCard = Math.floor(Math.random() * 11) + 1
-    secondCard = Math.floor(Math.random() * 11) + 1
+function playBlackJack(cards){
+    var cards = []
+    var firstCard = Math.floor(Math.random() * 11) + 1
+    var secondCard = Math.floor(Math.random() * 11) + 1
     cards.push(firstCard, secondCard)
-    cardsEl.innerText = "Cards: " + cards
+    cardsEl.innerText = "Cards: " + cards[0] + " " + cards[1]
+    var sum = cards[0] + cards[1]
+    counterEl.textContent = sum
+    if (sum <= 20) {
+        newLog.innerText = "Do you want to deal again?";
+        drawBtnEl.style.display = `inline`
+    } else if (sum === 21){
+        newLog.innerHTML = "Woohoo! You've got Blackjack!";
+        drawBtnEl.style.display = `none`
+        hasBlackjack = true
+    } else {
+        newLog.innerHTML = "Busted! You're out of the game!";
+        isGameOver = true;
+        drawBtnEl.style.display = `none`
+    }
+
+   return cards, sum
 }
 
-function drawNewCard(){
+function dealNewCard(cards, sum) {
     var newCard = Math.floor(Math.random() * 11) + 1
     cards.push(newCard)
     counterEl.textContent = sum += newCard
-    cardsEl.innerText = "Cards: " + cards
+    cardsEl.innerText = "Cards: " + cards[0] + " " + cards[1] + " " + cards[2]
+}
+
+function checkBlackjack(){
+    if (sum === 21) {
+        
+        startGame()
+    } 
 }
 
 
@@ -40,27 +56,7 @@ function hideGameBoard(){
     mainAppContainerEl.style.display = `block`
 }
 
-function playBlackJack(){
-    var cards = [firstCard, secondCard]
-    sum = cards[0] + cards[1]
-    counterEl.textContent = sum
-    if (sum <= 20) {
-        newLog.innerText = "Do you want to deal again?";
-        drawBtnEl.style.display = `inline`
-    } else if (sum === 21){
-        newLog.innerHTML = "Woohoo! You've got Blackjack!";
-        drawBtnEl.style.display = `none`
-    } else {
-        newLog.innerHTML = "Busted! You're out of the game!";
-        isGameOver = true;
-        drawBtnEl.style.display = `none`
-    }
-    
-
-}
-
 function startGame(){
     hideGameBoard()
-    dealCards()
     playBlackJack()
 }
